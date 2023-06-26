b4916288d0d241fd6e42bc93faf60de1a3b77faf
jaim23mf <108651419+jaim23mf@users.noreply.github.com>
2023-06-26 14:13:27 +0200

    Remove image

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -5,6 +5,8 @@ import { ReachService } from '../api_connection/api_reach/reach.service';
 import { GlobalConstants } from '../common/global-constants';
 import { MsgService } from '../msg.service';
 import { UsersService } from '../usersService/users.service';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-file-upload',
@@ -29,7 +31,7 @@ export class FileUploadComponent implements OnInit{
 
   api:string = GlobalConstants.api;
 
-  constructor(private http: HttpClient, private msg:MsgService,private userService:UsersService) {
+  constructor(private http: HttpClient, private msg:MsgService,private userService:UsersService,public confirm: MatDialog) {
 
    
   }
@@ -69,6 +71,33 @@ export class FileUploadComponent implements OnInit{
 
     }     
     
+  }
+
+
+  removeFile(){
+
+    if(this.fileName !="Upload " + this.requiredFileType.toUpperCase() + " file" && this.fileName != "" )
+    this.confirm
+    .open(ConfirmDialogComponent, {
+      data: 'You are going to delete this image.'
+    })
+    .afterClosed()
+    .subscribe((confirmado: Boolean) => {
+      if (confirmado) {
+        console.log(this.api + " - " +this.type+" - "+this.elemId);
+          let orden:Number = +this.elemId;
+          this.msg.sendText('',orden,+this.tipo);
+          this.fileName = "Upload " + this.requiredFileType.toUpperCase() + " file";
+      }
+    });
+   
+
+    /*const upload$ = this.http.delete(this.api+this.type+this.elemId);
+
+    upload$.subscribe({
+      next: this.handleUpdateResponse.bind(this),
+      error: this.handleError.bind(this)
+   });*/
   }
 
   handleUpdateResponse(data:any){
